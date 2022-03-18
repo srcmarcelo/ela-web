921291396f159fc3fad17dcc3a0be3105ec08a24
Marcelo Cristian <marcelo.cristian.969@gmail.com>
2022-03-17 23:30:56 -0300

    setting footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,6 +1,33 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDove, faPhone, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faInstagram, faWhatsapp } from "@fortawesome/free-brands-svg-icons";
 import { FooterContainer } from "./styles";
 
-const Footer = () => <FooterContainer>eu amo você morena</FooterContainer>;
+const Footer = () => (
+  <FooterContainer>
+    <div className="iconContainer">
+      ELA <FontAwesomeIcon icon={faDove} />
+    </div>
+    <div className="contactContainer">
+      <div className="content">
+        <div className="headers">Nos siga no Instagram!</div>
+        <FontAwesomeIcon icon={faInstagram} /> @educandariolereaprender
+      </div>
+      <div className="content">
+        <div className="headers">Contatos</div>
+        <div className="contacts">
+          <FontAwesomeIcon icon={faPhone} />  (87) 988175129
+        </div>
+        <div className="contacts">
+          <FontAwesomeIcon icon={faWhatsapp} />  (81) 988378335
+        </div>
+        <div className="contacts">
+          <FontAwesomeIcon icon={faEnvelope} />  ela@gmail.com
+        </div>
+      </div>
+    </div>
+  </FooterContainer>
+);
 
 export default Footer;
--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -3,5 +3,38 @@ import styled from "styled-components";
 export const FooterContainer = styled.div`
   background: linear-gradient(90deg, blue, darkblue);
   text-align: center;
-  height: 200px;
+  padding: 20px;
+
+  .iconContainer {
+    color: #fff;
+    font-size: 40px;
+  }
+
+  .contactContainer {
+    display: grid;
+    grid-auto-flow: column;
+    grid-template-columns: repeat(2, 1fr);
+    @media (max-width: 640px) {
+      grid-auto-flow: row;
+      grid-template-columns: none;
+    }
+
+    .content {
+      color: #fff;
+      font-size: 30px;
+
+      @media (max-width: 450px) {
+        font-size: 20px;
+      }
+
+      .headers {
+        color: #fff;
+        font-size: 40px;
+      }
+
+      .contacts {
+        font-size: 24px;
+      }
+    }
+  }
 `;
